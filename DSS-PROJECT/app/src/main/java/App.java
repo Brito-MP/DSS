98b3ed1741df6498ea34422a6ccbc6424328d83e
SimaocCampos <a107321@alunos.uminho.pt>
2026-01-11 23:09:59 +0000

    aa

--- DSS-PROJECT/app/src/main/java/App.java
+++ DSS-PROJECT/app/src/main/java/App.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
+import data.FuncionarioDAO;
+import data.PedidoDAO;
+import data.PostoDAO;
 import model.InterRestauranteL;
 import model.RestauranteFacade;
 import model.gestao.Alimento;
@@ -28,7 +31,10 @@ public class App {
 
     // ========== MAIN E CONSTRUTOR ==========
     public static void main(String[] args) {
-        //PedidoDAO.getInstance().clear(); // LIMPAR A BASE DE DADOS
+        
+        //PedidoDAO.getInstance().clear();
+        //PostoDAO.getInstance().clear();
+        //FuncionarioDAO.getInstance().clear();
 
         App app = new App();
         app.run();
@@ -44,17 +50,19 @@ public class App {
         NewMenu menu = new NewMenu(new String[] {
                 "TestaDAO",
                 "Funcionario",
-                "Cliente"//,
-                //"Sair"
+                "Cliente"// ,
+                // "Sair"
         });
 
         menu.setHandler(1, this::testaDAO);
         menu.setHandler(2, this::menuFuncionario);
         menu.setHandler(3, this::menuCliente);
-        /* menu.setHandler(4, () -> {
-            System.out.println("Até já!");
-            System.exit(0);
-        }); */
+        /*
+         * menu.setHandler(4, () -> {
+         * System.out.println("Até já!");
+         * System.exit(0);
+         * });
+         */
 
         menu.run();
         scanner.close();
@@ -215,7 +223,7 @@ public class App {
                 System.out.println("Alimento ID: " + entry.getKey() + " | Quantidade: " + entry.getValue());
             }
         });
-        
+
         menu.setHandler(3, () -> displayPostosLivres());
         menu.setHandler(4, () -> registarFuncionario());
 
@@ -257,7 +265,7 @@ public class App {
             mostrarPedidoDetalhado(pedido);
             System.out.println();
         }
-        
+
         String[] opcoes = new String[pendentes.size()];
         for (int i = 0; i < pendentes.size(); i++) {
             Pedido pedido = pendentes.get(i);
